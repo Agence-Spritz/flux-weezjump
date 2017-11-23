a9e63dba0fe9b7ff604e782503fac3073f2bd7e6
Sébastien PAQUET <cavallinox@MacBook-Pro-de-Sebastien.local>
2017-11-23 17:14:21 +0100

    Ajout js pour notifications

--- web/admin/js/js-spritz.js
+++ web/admin/js/js-spritz.js
@@ -20,3 +20,33 @@ $(document).on('click', '.number-spinner button', function () {
 	}
 	btn.closest('.number-spinner').find('input').val(newVal);
 });
+
+// Système de notification, messages d'alerte
+var handleGritterNotification = function() {
+	
+	$('#add-without-image').click(function(){
+		$.gritter.add({
+			title: 'Message de notification, à personnaliser dans js-spritz',
+			text: 'This will fade out after a certain amount of time.'
+		});
+		$("#myModal").modal('hide');
+		return false;
+	});
+	
+	$("#remove-all").click(function(){
+		$.gritter.removeAll();
+		return false;
+	});
+	
+};
+
+
+var Notification = function () {
+	"use strict";
+    return {
+        //main function
+        init: function () {
+            handleGritterNotification();
+        }
+    };
+}();
